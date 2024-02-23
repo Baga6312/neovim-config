83356173cdcb1a5c2c251ec557ffb3e26dd9d7a2
Baga6312 <magroubertn@gmail.com>
2024-02-23 13:11:20 +0100

    fixed some errors

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,40 +1,52 @@
-local opts = {noremap = true , silent = true} 
-local term_opts = {silent = true} 
+local opts = { noremap = true, silent = true }
 
-local keymap = vim.api.nvim_se-_keymap
+local term_opts = { silent = true }
 
--- remap space as a leader -- 
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
 
-keymap( "" , "<Space>" , "<Nop>" , opts)
-vim.g.mapleader = " " 
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- explorer 
-keymap("n" , "<leader>e" , ":Lex 30<cr>" , opts) 
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
 
--- window navigation 
-keymap("n" , "<C-h>" , "<C-w>h" , opts) 
-keymap("n" , "<C-j>" , "<C-w>j" , opts) 
-keymap("n" , "<C-k>" , "<C-w>k" , opts) 
-keymap("n" , "<C-l>" , "<C-w>l" , opts) 
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
--- resize with arrows 
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
-keymap("n" , "<C-up>", ":resize +2<CR>" , opts)
-keymap("n" , "<C-down>", ":resize -2<CR>" , opts)
-keymap("n" , "<C-left>", ":vertical resize -2<CR>" , opts)
-keymap("n" , "<C-right>", ":vertical resize +2<CR>" , opts)
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
--- navigation buffer 
-keymap("n" , "<S-l>", ":bnext<CR>" , opts) 
-keymap("n" , "<S-h>" , "bprevieous" , opts) 
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
--- INSERT -- 
--- indent mode 
-keymap("v", "<" , "<gv" , opts)  
-keymap("v", ">" , ">gv" , opts) 
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
 
--- move text up and down 
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,46 +1,46 @@
-local options = { 	
-	bk = false ,		 -- backup , create a backup file 
-	cb = 'unamedplus' ,      -- clipboard , the system clipboard  
-	ch = 1 , 		 -- cmdheight , space needed in the neovim displaying msg
-	cot = {menu , menuone ,preview , noselect} , -- completeopt , options suggestion when u search for something   
-	cole = 0 , 		 -- conceallevel , text is shown as normal  	
-	enc = "utf-8",            -- encoding type 
-	hls = true,		 -- highlighting the search pattern 
-	hi = 1000 , 		 -- history search counter 
-	ic = false,		 -- ignore case 	
-	mouse = "a", 		 -- enable mouse support  
-	ph = 8  ,		 -- pop up menu height 
-	smd = true , 		 -- we dont need to see -- INSERT -- anymore ik but i like it 	
-	stal = 1,		 -- show tha tabulation line up 
-	scs = true, 		 -- smart case 
-	si = true ,		 -- smart indenting 
-	sb = true ,		 -- when splitting horizentatlly make sure the new tab is on the bottom  
-	sr = true,		 -- same thing but when splitting verticaly the new tab will be on the right  
-	swf = true,		 -- create a swap file  
-	tgc = true, 		 -- set terminal gui colors 
-	tm = 1000 ,		 -- time for a mapped squence to be completed 
-	udf = true, 		 -- persistence to undo changes on files 
-	ut = 300 ,		 -- faster completion time 
-	et = true, 		 -- allowen to insert tab space 
-	sw = 3 	,		 -- number of spaces inserted for each identation 
-	ts = 3 	,		 -- insert 3 spaces for a tab 
-	cursorline = true ,		 -- highlight the cursor 
-	nu = true ,		 -- set numbered lines 
-	rnu = true, 		 -- set relative number lines 
-	nuw = 2 ,		 -- set the nulber of column width to 2 
-	scl = "yes", 		 -- always show the sign column, otherwise it would shift the text each time
-	wrap = false, 		 -- display lines as one long line 
-	so = 8 	,		 
-	siso = 8 ,		 
-	guifont = "monospace:h17" , 
-	--shm:append "c", 
-
-
-
-
+local options = {
+  backup = false,                          -- creates a backup file
+  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  conceallevel = 0,                        -- so that `` is visible in markdown files
+  fileencoding = "utf-8",                  -- the encoding written to a file
+  hlsearch = true,                         -- highlight all matches on previous search pattern
+  ignorecase = true,                       -- ignore case in search patterns
+  mouse = "a",                             -- allow the mouse to be used in neovim
+  pumheight = 10,                          -- pop up menu height
+  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+  showtabline = 2,                         -- always show tabs
+  smartcase = true,                        -- smart case
+  smartindent = true,                      -- make indenting smarter again
+  splitbelow = true,                       -- force all horizontal splits to go below current window
+  splitright = true,                       -- force all vertical splits to go to the right of current window
+  swapfile = false,                        -- creates a swapfile
+  -- termguicolors = true,                    -- set term gui colors (most terminals support this)
+  timeoutlen = 1000,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true,                         -- enable persistent undo
+  updatetime = 300,                        -- faster completion (4000ms default)
+  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true,                        -- convert tabs to spaces
+  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
+  tabstop = 2,                             -- insert 2 spaces for a tab
+  cursorline = true,                       -- highlight the current line
+  number = true,                           -- set numbered lines
+  relativenumber = false,                  -- set relative numbered lines
+  numberwidth = 4,                         -- set number column width to 2 {default 4}
+  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+  wrap = false,                            -- display lines as one long line
+  scrolloff = 8,                           -- is one of my fav
+  sidescrolloff = 8,
+  guifont = "monospace:h17",               -- the font used in graphical neovim applications
 }
 
-for k , v in pairs(options) do 
-	vim.opt[k] = v 
-end 
+vim.opt.shortmess:append "c"
 
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
