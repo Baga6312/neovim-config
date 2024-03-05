2ad3a810993510b252a33bd12deb3a00c908ccc9
Baga6312 <magroubertn@gmail.com>
2024-03-05 12:47:04 +0100

    added ast-grep

--- lua/user/lsp_config.lua
+++ lua/user/lsp_config.lua
@@ -1,8 +1,9 @@
 require "mason".setup()
 require "mason-lspconfig".setup({
-  ensure_installed = {"lua_ls" , "pylsp" }
+  ensure_installed = {"lua_ls" , "pylsp" , "ast_grep" }
 })
 
 
 require "lspconfig".sumneko_lua.setup {}
 require'lspconfig'.pylsp.setup{}
+require'lspconfig'.ast_grep.setup{}
